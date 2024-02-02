9dd6469cb8da3d4fe6d13016a6f32fcab8d4c7c6
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-02 14:01:28 +0000

    added array of questions for user.

--- index.js
+++ index.js
@@ -4,9 +4,55 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
+const questions = [{
+    type: 'input',
+    message: 'Project Title:',
+    name: 'title'
+},
+{
+    type: 'input',
+    message: 'Description:',
+    name: 'description'
+}, 
+{
+    type: 'input',
+    message: 'Installation:',
+    name: 'installation'
+},
+{
+    type: 'input',
+    message: 'Usage:',
+    name: 'usage'
+},
+{
+    type: 'list',
+    message: 'License:',
+    name: 'license',
+    choices: ['MIT', 'Apache 2.0', 'GNU General Public License v2.0']
+},
+{
+    type: 'input',
+    message: 'Contributing:',
+    name: 'contributing'
+},
+{
+    type: 'input',
+    message: 'Tests:',
+    name: 'tests'
+},
+{
+    type: 'input',
+    message: 'Github username:',
+    name: 'username'
+},
+{
+    type: 'input',
+    message: 'Email address:',
+    name: 'email'
+}
 ];
+
+inquirer.prompt(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
